fd412afc5c0634cdf6fbed0e477d5c97df199af5
Mattèo Gauthier <matteo.gauthier@gmail.com>
2021-07-13 01:07:36 +0200

    ga.js + meta description

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document"
-
+import Script from "next/script"
 class MyDocument extends Document {
   render() {
     return (
@@ -12,9 +12,20 @@ class MyDocument extends Document {
           <meta name="msapplication-config" content="/browserconfig.xml" />
           <meta name="msapplication-TileColor" content="#00003e" />
           <meta name="theme-color" content="#00003e" />
+          <meta name="description" content="Looking for a DEX, a swaping platform on the Kucoin Community Chain ? Find out KCC dApps of your need :D"/>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;500;600;700&display=swap" rel="stylesheet" />
+          <Script async strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-VPKNXPXVTQ" />
+          <Script strategy="afterInteractive">
+            {`<!-- Global site tag (gtag.js) - Google Analytics -->
+ 							 window.dataLayer = window.dataLayer || [];
+ 							 function gtag(){dataLayer.push(arguments);}
+ 							 gtag('js', new Date());
+
+ 							 gtag('config', 'G-VPKNXPXVTQ');
+						`}
+          </Script>
         </Head>
         <body>
           <Main />
