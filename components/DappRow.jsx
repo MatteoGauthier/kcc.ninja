d3ea8185979805869cff2caab581045753d0d371
Mattèo Gauthier <matteo.gauthier@gmail.com>
2021-07-12 12:39:58 +0200

    Adding explorer link to

--- components/DappRow.jsx
+++ components/DappRow.jsx
@@ -15,7 +15,7 @@ function getDappsTags(el) {
 		case f("DEX"):
 			tagsElms.push(<span key="DEX" className=" text-emerald-300">DEX</span>);
 		case f("Farm"):
-			tagsElms.push(<span key="Farm" className=" text-yellow-300">Farm</span>);
+			tagsElms.push(<span key="Farm" className="text-yellow-300 ">Farm</span>);
 
 		default:
 			break;
@@ -26,25 +26,25 @@ function getDappsTags(el) {
 function DappRow({ dapp, variants }) {
 	return (
 		<>
-			<motion.div variants={variants} className=" block h-full w-full  card">
-				<div className="bg-black/60 rounded-t-xl py-1 flex px-3 items-center justify-between">
-					<div className="flex space-x-3 text-xs items-center">{getDappsTags(dapp)}</div>
-					<div className="flex space-x-4 items-center text-white">
-						<a className="inline-flex text-sm space-x-1 items-center " href={"https://twitter.com/" + dapp.Twitter}>
+			<motion.div variants={variants} className="block w-full h-full card">
+				<div className="flex items-center justify-between px-3 py-1 bg-black/60 rounded-t-xl">
+					<div className="flex items-center space-x-3 text-xs">{getDappsTags(dapp)}</div>
+					<div className="flex items-center space-x-4 text-white">
+						<a className="inline-flex items-center space-x-1 text-sm " href={"https://twitter.com/" + dapp.Twitter}>
 							<TwitterIcon /> <span> {dapp.Twitter}</span>
 						</a>
-						<a className="inline-flex text-sm space-x-1 items-center " href={"https://twitter.com/" + dapp.Twitter}>
-							<IconLayer /> <span>Explorer</span>
+						<a className="inline-flex items-center space-x-1 text-sm " href={dapp.ExplorerTokenContractAdress}>
+							<IconLayer /> <span>Token Explorer</span>
 						</a>
 					</div>
 				</div>
-				<a href={createUrl(dapp.Url)} className="flex-row flex justify-between px-3 py-2 items-center text-white">
+				<a href={createUrl(dapp.Url)} className="flex flex-row items-center justify-between px-3 py-2 text-white">
 					<div className="flex flex-col">
 						<h3 className="text-lg font-medium">{dapp.Name}</h3>
 						<p className="text-sm text-gray-400 line-clamp-2">{dapp.Description}</p>
 					</div>
-					{/* <div className="h-full flex-1">
-						<ExternalLinkIcon className="h-10 w-10 text-light-blue-300/90" />
+					{/* <div className="flex-1 h-full">
+						<ExternalLinkIcon className="w-10 h-10 text-light-blue-300/90" />
 					</div> */}
 				</a>
 			</motion.div>
