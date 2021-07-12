bdb13edc13530f18bc600b5fc234d90e80f3ab04
Mattèo Gauthier <matteo.gauthier@gmail.com>
2021-07-13 00:20:24 +0200

    adding eslint rules, fix color issue ..... purging ahhhhh, bump full-palette

--- libs/utils.js
+++ libs/utils.js
@@ -1,28 +1,36 @@
 export function getDappsTags(el) {
-	const f = (term) => {
-		if (el.Tags.includes(term)) {
-			return el.tags;
-		}
-	};
-	let tagsElms = [];
-	switch (el.tags) {
-		case f("DEX"):
-			tagsElms.push(<span key="DEX" className=" text-emerald-300">DEX</span>);
-		case f("Farm"):
-			tagsElms.push(<span key="Farm" className=" text-yellow-300">Farm</span>);
+  const f = (term) => {
+    if (el.Tags.includes(term)) {
+      return el.tags
+    }
+  }
+  let tagsElms = []
+  switch (el.tags) {
+    case f("DEX"):
+      tagsElms.push(
+        <span key="DEX" className="text-emerald-300">
+          DEX
+        </span>
+      )
+    case f("Farm"):
+      tagsElms.push(
+        <span key="Farm" className="text-yellow-300">
+          Farm
+        </span>
+      )
 
-		default:
-			break;
-	}
-    return tagsElms
+    default:
+      break
+  }
+  return tagsElms
 }
 
 export function createUrl(string) {
-	// validate string as url
-	let url;
-	if (string.match(/^(http|https):\/\//i) === null) {
-		url = "http://" + string;
-	}
+  // validate string as url
+  let url
+  if (string.match(/^(http|https):\/\//i) === null) {
+    url = "http://" + string
+  }
 
-	return url;
+  return url
 }
