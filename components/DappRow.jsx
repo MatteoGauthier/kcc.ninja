cca9c84b5dd8f0c9b4c58477fed56014c7173719
Mattèo Gauthier <matteo.gauthier@gmail.com>
2021-07-12 23:42:37 +0200

    Adding next-seo + reorganizing components

--- components/DappRow.jsx
+++ components/DappRow.jsx
@@ -3,29 +3,42 @@ import ExternalLinkIcon from "@heroicons/react/outline/ExternalLinkIcon";
 import TwitterIcon from "./svg/TwitterIcon";
 import IconLayer from "./svg/IconLayer";
 import { motion } from "framer-motion";
-import createUrl from "../libs/url";
-function getDappsTags(el) {
-	const f = (term) => {
-		if (el.Tags.includes(term)) {
-			return el.tags;
-		}
-	};
-	let tagsElms = [];
-	switch (el.tags) {
-		case f("DEX"):
-			tagsElms.push(<span key="DEX" className=" text-emerald-300">DEX</span>);
-		case f("Farm"):
-			tagsElms.push(<span key="Farm" className=" text-yellow-300">Farm</span>);
-
-		default:
-			break;
-	}
-    return tagsElms
-}
+import { createUrl, getDappsTags } from "../libs/utils";
+import { NextSeo } from "next-seo";
 
 function DappRow({ dapp, variants }) {
 	return (
 		<>
+			<NextSeo
+				title={"kcc.ninja - Curated list of Kucoin Community Chain dApps"}
+				openGraph={{
+					url: "https://kcc.ninja",
+					title: "kcc.ninja - Curated list of Kucoin Community Chain dApps",
+					description: "Looking for a DEX, a swaping platform on the Kucoin Community Chain ? Find out KCC dApps of your need :D",
+					images: [
+						{
+							url: "https://www.example.ie/og-image-01.jpg",
+							width: 800,
+							height: 600,
+							alt: "Og Image Alt",
+						},
+						{
+							url: "https://www.example.ie/og-image-02.jpg",
+							width: 900,
+							height: 800,
+							alt: "Og Image Alt Second",
+						},
+						{ url: "https://www.example.ie/og-image-03.jpg" },
+						{ url: "https://www.example.ie/og-image-04.jpg" },
+					],
+					site_name: "kcc.ninja",
+				}}
+				twitter={{
+					handle: "@matteogauthier_",
+					site: "@matteogauthier_",
+					cardType: "summary_large_image",
+				}}
+			/>
 			<motion.div variants={variants} className=" block h-full w-full  card">
 				<div className="bg-black/60 rounded-t-xl py-1 flex px-3 items-center justify-between">
 					<div className="flex space-x-3 text-xs items-center">{getDappsTags(dapp)}</div>
--- libs/url.js
+++
@@ -1,9 +0,0 @@
-export default function createUrl(string) {
-	// validate string as url
-	let url;
-	if (string.match(/^(http|https):\/\//i) === null) {
-		url = "http://" + string;
-	}
-
-	return url;
-}
