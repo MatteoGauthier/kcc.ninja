065afe331ab39780113648d773efd22ee2a9c673
Mattèo Gauthier <matteo.gauthier@gmail.com>
2021-07-12 23:56:02 +0200

    seo improvment prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,24 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require("tailwindcss/defaultTheme")
 
 module.exports = {
-	mode: "jit",
-	purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-	darkMode: false, // or 'media' or 'class'
-	theme: {
-		extend: {
-			fontFamily: {
-				sans: ["'Work Sans'", "Work Sans", ...defaultTheme.fontFamily.sans],
-			},
-			colors: {
-				primary: "#49ffa1",
-				secondary: "#ffc107",
-				tertiary: "#ffc107",
-			},
-		},
-	},
-	variants: {
-		extend: {},
-	},
-	plugins: [
-		require('@tailwindcss/line-clamp'),
-	],
-	presets:[
-		require('full-palette')
-	]
-};
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["'Work Sans'", "Work Sans", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: "#49ffa1",
+        secondary: "#ffc107",
+        tertiary: "#ffc107",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/line-clamp")],
+  presets: [require("full-palette")],
+}
